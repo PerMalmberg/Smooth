5133f1b1c71399039893f4ccd0a676314528f56c
Per Malmberg <per.malmberg@gmail.com>
2017-08-20 17:13:15 +0200

    BME280 mode and oversampling can be configured and read.

--- application/sensor/BME280.cpp
+++ application/sensor/BME280.cpp
@@ -6,6 +6,8 @@
 #include <vector>
 #include <sstream>
 #include <smooth/application/sensor/BME280.h>
+#include <smooth/core/util/FixedBuffer.h>
+#include <smooth/core/util/ByteSet.h>
 #include "esp_log.h"
 
 namespace smooth
@@ -21,11 +23,52 @@ namespace smooth
 
             uint8_t BME280::read_id()
             {
-                std::vector<uint8_t> data;
-                data.reserve(1);
-                data.push_back(0);
-                auto res = read(address, 0xD0, data);
+                core::util::FixedBuffer<uint8_t, 1> data;
+                auto res = read(address, ID_REG, data);
                 return res ? data[0] : 0;
+            }
+
+            bool BME280::configure_sensor(SensorMode mode,
+                                          OverSampling humidity,
+                                          OverSampling pressure,
+                                          OverSampling temperature)
+            {
+                std::vector<uint8_t> data;
+
+                data.push_back(CTRL_HUM_REG);
+                data.push_back(humidity);
+
+                data.push_back(CTRL_MEAS_REG);
+                uint8_t meas_data = (temperature << 5);
+                meas_data |= (pressure << 2);
+                meas_data |= mode;
+                data.push_back(meas_data);
+
+                return write(address, data, true);
+            }
+
+            bool BME280::read_configuration(SensorMode& mode,
+                                            OverSampling& humidity,
+                                            OverSampling& pressure,
+                                            OverSampling& temperature)
+            {
+                core::util::FixedBuffer<uint8_t, 1> hum;
+                core::util::FixedBuffer<uint8_t, 1> rest;
+
+                bool res = read(address, CTRL_HUM_REG, hum);
+                res &= read(address, CTRL_MEAS_REG, rest);
+
+                if (res)
+                {
+                    core::util::ByteSet h(hum[0]);
+                    humidity = static_cast<OverSampling>(h.get_bits_as_byte(0, 2));
+
+                    core::util::ByteSet r(rest[0]);
+                    mode = static_cast<SensorMode>(r.get_bits_as_byte(0, 1));
+                    pressure = static_cast<OverSampling>(r.get_bits_as_byte(2, 4));
+                    temperature = static_cast<OverSampling>(r.get_bits_as_byte(5, 7));
+                }
+                return res;
             }
 
         }
--- include/smooth/application/sensor/BME280.h
+++ include/smooth/application/sensor/BME280.h
@@ -18,10 +18,50 @@ namespace smooth
                     : public core::io::i2c::I2CMasterDevice
             {
                 public:
-                    BME280(i2c_port_t port, core::ipc::Mutex& guard, uint8_t address);
+                    BME280(i2c_port_t port, core::ipc::Mutex& guard, uint8_t address1);
 
                     uint8_t read_id();
+
+                    enum SensorMode
+                    {
+                        Sleep = 0x00,
+                        Forced = 0x01,
+                        Normal = 0x02
+                    };
+
+                    enum OverSampling
+                    {
+                        Skipped = 0x00,
+                        Oversamplingx1 = 0x01,
+                        Oversamplingx2 = 0x02,
+                        Oversamplingx4 = 0x03,
+                        Oversamplingx8 = 0x04,
+                        Oversamplingx16 = 0x05
+                    };
+
+                    bool configure_sensor(SensorMode mode, OverSampling humidity, OverSampling pressure, OverSampling temperature);
+                    bool read_configuration(SensorMode& mode, OverSampling& humidity, OverSampling& pressure, OverSampling& temperature);
+
+
                 private:
+                    const uint8_t HUM_LSB_REG = 0xFE;
+                    const uint8_t HUM_MSB_REG = 0xFD;
+                    const uint8_t TEMP_XLSB_REG = 0xFC;
+                    const uint8_t TEMP_LSB_REG = 0xFB;
+                    const uint8_t TEMP_MSB_REG = 0xFA;
+                    const uint8_t PRESS_XLSB_REG = 0xF9;
+                    const uint8_t PRESS_LSB_REG = 0xF8;
+                    const uint8_t PRESS_MSB_REG = 0xF7;
+                    const uint8_t CONFIG_REG = 0xF5;
+                    const uint8_t CTRL_MEAS_REG = 0xF4;
+                    const uint8_t STATUS_REG = 0xF3;
+                    const uint8_t CTRL_HUM_REG = 0xF2;
+                    const uint8_t CAL_41_REG = 0xF0;
+                    const uint8_t CAL_26_REG = 0xE1;
+                    const uint8_t RESET_REG = 0xE0;
+                    const uint8_t ID_REG = 0xD0;
+                    const uint8_t CAL_25_REG = 0xA1;
+                    const uint8_t CAL_00_REG = 0x88;
                     uint8_t address;
             };
         }
