f7daaf088e1cb4df8ebe27cf6bd05d9937cd5b0a
Per Malmberg <per.malmberg@gmail.com>
2017-12-20 23:08:14 +0100

    Implemented File class. Added CommandDispatcher and some test commands.

---
+++ core/filesystem/File.cpp
@@ -0,0 +1,69 @@
+#include <fstream>
+#include <utility>
+#include <smooth/core/filesystem/File.h>
+#include <smooth/core/logging/log.h>
+
+using namespace smooth::core::logging;
+
+namespace smooth
+{
+    namespace core
+    {
+        namespace filesystem
+        {
+            smooth::core::filesystem::File::File(std::string name)
+                    : name(std::move(name))
+            {
+            }
+
+            bool File::read(std::vector<uint8_t>& data) const
+            {
+                bool res = false;
+
+                try
+                {
+                    std::fstream fs{name, std::ios::binary | std::ios::in};
+                    if (fs.is_open())
+                    {
+                        auto size = fs.seekg(0, fs.end).tellg();
+                        fs.seekg(0, fs.beg);
+                        data.resize(static_cast<unsigned long>(size));
+                        res = fs.read(reinterpret_cast<char*>(data.data()), size).gcount() == size;
+                    }
+                }
+                catch (std::exception& ex)
+                {
+                    Log::error("File", Format("Error reading file: {1}", Str(ex.what())));
+                }
+
+                return res;
+            }
+
+            bool File::write(const std::string& data) const
+            {
+                return write(reinterpret_cast<const uint8_t*>(data.data()), data.size());
+            }
+
+            bool File::write(const uint8_t* data, size_t length) const
+            {
+                bool res = false;
+
+                try
+                {
+                    std::fstream fs{name, std::ios::binary | std::ios::out | std::ios::trunc};
+                    if (fs.is_open())
+                    {
+                        fs.write(reinterpret_cast<const char*>(data), length);
+                    }
+                }
+                catch (std::exception& ex)
+                {
+                    Log::error("File", Format("Error reading file: {1}", Str(ex.what())));
+                }
+
+                return res;
+            }
+
+        }
+    }
+}
---
+++ include/smooth/core/filesystem/File.h
@@ -0,0 +1,39 @@
+#pragma once
+
+
+#include <vector>
+#include <string>
+
+namespace smooth
+{
+    namespace core
+    {
+        namespace filesystem
+        {
+            /// Utility class to read/write an entire file.
+            class File
+            {
+                public:
+                    /// \param name The full path and name of the file.
+                    explicit File(std::string name);
+                    /// Reads the entire file contents into the vector.
+                    /// \param data The target container
+                    /// \return true on success, false on failure
+                    bool read(std::vector<uint8_t>& data) const;
+
+                    /// Writes the entire vector to the file, overwriting any existing file.
+                    /// \param data The source container
+                    /// \return true on success, false on failure
+                    bool write(const std::string& data) const;
+
+                    /// /// Writes the provided data to the file, overwriting any existing file.
+                    /// \param data The data
+                    /// \param length The length
+                    /// \return true on success, false on failure
+                    bool write(const uint8_t* data, size_t length) const ;
+                private:
+                    std::string name;
+            };
+        }
+    }
+}
