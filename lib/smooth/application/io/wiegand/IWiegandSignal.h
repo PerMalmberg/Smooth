14309eeff9a8db2f00ef3e95d02058c857cc4969
Per Malmberg <per.malmberg@gmail.com>
2019-02-05 22:57:15 +0100

    Updates Wiegand (and placed them in the correct folders)

--- lib/smooth/application/io/wiegand/IWiegandSignal.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-class IWiegandSignal
-{
-    public:
-        virtual void wiegand_number(uint8_t num) = 0;
-        virtual void wiegand_id(uint32_t id, uint8_t byte_count) = 0;
-};
---
+++ lib/smooth/include/smooth/application/io/wiegand/IWiegandSignal.h
@@ -0,0 +1,20 @@
+#pragma once
+
+namespace smooth
+{
+    namespace application
+    {
+        namespace io
+        {
+            namespace wiegand
+            {
+                class IWiegandSignal
+                {
+                    public:
+                        virtual void wiegand_number(uint8_t num) = 0;
+                        virtual void wiegand_id(uint32_t id, uint8_t byte_count) = 0;
+                };
+            }
+        }
+    }
+}
