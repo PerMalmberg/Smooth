bad2ca73e9d36bf8dd8454fcb2dbe1cf0d64830f
Per Malmberg <per.malmberg@gmail.com>
2019-06-01 12:40:46 +0200

    #31 - WiP - fails to link against pre-build IDF libs.

--- lib/smooth/idf_utils.cmake
+++ lib/smooth/idf_utils.cmake
@@ -1,3 +1,5 @@
+include($ENV{IDF_PATH}/tools/cmake/idf.cmake)
+
 function(smooth_verify_idf_path)
     if (NOT EXISTS "$ENV{IDF_PATH}/Kconfig")
         message(FATAL_ERROR "Environment IDF_PATH must be set to an existing IDF-installation. IDF_PATH: '$ENV{IDF_PATH}'")
@@ -6,29 +8,46 @@ function(smooth_verify_idf_path)
     endif ()
 endfunction()
 
-function(smooth_configure_link target)
+set(smooth_req_comps
+        esp_wifi
+        esp32
+        freertos
+        esptool_py
+        fatfs
+        sdmmc
+        spi_flash
+        nvs_flash
+        wear_levelling
+        libsodium
+        lwip
+        json
+        mbedtls
+        xtensa)
+
+function(smooth_link_to_idf target)
     if(${CMAKE_CXX_COMPILER} MATCHES "xtensa")
-        message(STATUS "Smooth: Importing IDF components.")
+        foreach(c ${smooth_req_comps})
+            target_link_libraries(${target} idf::${c})
+        endforeach()
+    endif()
+endfunction()
 
-        include($ENV{IDF_PATH}/tools/cmake/idf.cmake)
-        target_compile_definitions(smooth PUBLIC ESP_PLATFORM)
 
-        set(comps fatfs sdmmc spi_flash nvs_flash wear_levelling libsodium lwip json)
+function(smooth_setup target)
+    if(${CMAKE_CXX_COMPILER} MATCHES "xtensa")
+        target_compile_definitions(${target} PUBLIC ESP_PLATFORM)
 
-        idf_build_component("${CMAKE_CURRENT_LIST_DIR}/smooth_idf_component")
+        idf_build_component("${CMAKE_SOURCE_DIR}/lib/smooth/smooth_idf_component")
 
         idf_build_process(esp32
-                COMPONENTS esp32 freertos esptool_py ${comps}
+                COMPONENTS ${smooth_req_comps}
                 SDKCONFIG ${CMAKE_CURRENT_LIST_DIR}/sdkconfig
                 BUILD_DIR ${CMAKE_BINARY_DIR})
 
-
-        foreach(c ${comps})
-            target_link_libraries(${target} idf::${c})
-        endforeach()
+        smooth_link_to_idf(${target})
     else()
         message(STATUS "Smooth: Building for native Linux.")
-        target_include_directories(${target} PRIVATE $ENV{IDF_PATH}/components/json/cJSON)
+        target_include_directories(smooth PRIVATE $ENV{IDF_PATH}/components/json/cJSON)
 
         target_link_libraries(${target} pthread mbedtls mbedx509 mbedcrypto)
 
