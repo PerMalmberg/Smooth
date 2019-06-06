5e50f1e7af449ecbfc7dd7c3bb83dcd41bb8820c
Per Malmberg <per.malmberg@gmail.com>
2019-06-06 20:45:02 +0200

    Run FreeRTOS on single core until cache issue in toolchain is fixed. https://www.esp32.com/viewtopic.php?f=2&t=10727

---
+++ flash.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+pushd `pwd`
+mkdir -p build
+cd build
+cmake .. -DESP_PLATFORM=1 -DCMAKE_TOOLCHAIN_FILE=$IDF_PATH/tools/cmake/toolchain-esp32.cmake -DTARGET=esp32 && \
+make -j 8 && \
+python $IDF_PATH/components/esptool_py/esptool/esptool.py --baud 921600 -p /dev/ttyUSB1 write_flash @flash_project_args && \
+popd && \
+python $IDF_PATH/tools/idf_monitor.py -p /dev/ttyUSB1 ./build/smooth_test
+
