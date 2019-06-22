b804cc0908463ae6b2f8c3cbbe223b0a109f71c5
Per Malmberg <per.malmberg@gmail.com>
2019-06-22 19:44:08 +0200

    #36 - Prevent move operations on Tasks.

--- lib/smooth/include/smooth/core/Task.h
+++ lib/smooth/include/smooth/core/Task.h
@@ -21,99 +21,101 @@
 #include <atomic>
 
 #ifdef ESP_PLATFORM
-    #include <freertos/FreeRTOS.h>
-    #include <freertos/task.h>
-    #include <sdkconfig.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <sdkconfig.h>
 
-    #if CONFIG_FREERTOS_HZ < 1000
-        #error "Smooth requires CONFIG_FREERTOS_HZ at 1000Hz for proper functionality (spec. timers)"
-    #endif
+#if CONFIG_FREERTOS_HZ < 1000
+#error "Smooth requires CONFIG_FREERTOS_HZ at 1000Hz for proper functionality (spec. timers)"
+#endif
 #else
-    const int tskNO_AFFINITY = std::numeric_limits<int>::max();
+const int tskNO_AFFINITY = std::numeric_limits<int>::max();
 #endif
 
-namespace smooth
+namespace smooth::core
 {
-    namespace core
+    /// The Task class encapsulates management and execution of a task.
+    /// The intent is to provide the scaffolding needed by nearly every task in an
+    /// embedded system; an initialization method, a periodically called tick(),
+    /// the ability to receive events in a thread-safe manner.
+    class Task
     {
-        /// The Task class encapsulates management and execution of a task.
-        /// The intent is to provide the scaffolding needed by nearly every task in an
-        /// embedded system; an initialization method, a periodically called tick(),
-        /// the ability to receive events in a thread-safe manner.
-        class Task
-        {
-            public:
-                virtual ~Task();
+        public:
+            virtual ~Task();
 
-                /// Starts the task.
-                void start();
+            /// Starts the task.
+            void start();
 
-                void register_queue_with_task(smooth::core::ipc::ITaskEventQueue *task_queue);
-                void register_polled_queue_with_task(smooth::core::ipc::IPolledTaskQueue *polled_queue);
+            void register_queue_with_task(smooth::core::ipc::ITaskEventQueue* task_queue);
 
-                void unregister_polled_queue_with_task(smooth::core::ipc::IPolledTaskQueue *polled_queue);
+            void register_polled_queue_with_task(smooth::core::ipc::IPolledTaskQueue* polled_queue);
 
-                Task(const Task&) = delete;
+            void unregister_polled_queue_with_task(smooth::core::ipc::IPolledTaskQueue* polled_queue);
 
-                Task& operator=(const Task&) = delete;
+            Task(const Task&) = delete;
 
-            protected:
+            Task& operator=(const Task&) = delete;
 
-                /// Use this constructor to attach to an existing task, i.e. the main task.
-                /// \param task_to_attach_to The task to attach to.
-                /// \param priority Task priority
-                /// \param tick_interval Tick interval
-                Task(uint32_t priority, std::chrono::milliseconds tick_interval);
+            Task(Task&&) = delete;
 
-                /// Use this constructor when creating your own task.
-                /// \param task_name Name of task.
-                /// \param stack_size Tack size, in bytes.
-                /// \param priority Task priority
-                /// \param tick_interval Tick interval
-                /// \param core Core affinity, defaults to no affinity
-                Task(std::string  task_name,
-                     uint32_t stack_size,
-                     uint32_t priority,
-                     std::chrono::milliseconds tick_interval,
-                     int core = tskNO_AFFINITY);
+            Task& operator=(Task&&) = delete;
 
-                /// The tick() method is where the task shall perform its work.
-                /// It is called every 'tick_interval' when there no events available.
-                /// Note that if there is a constant stream of event received via a TaskEventQueue,
-                /// the tick may be delayed (depending on the tick_interval).
-                virtual void tick()
-                {
-                }
+        protected:
 
-                /// Called once when task is started.
-                virtual void init()
-                {
-                }
+            /// Use this constructor to attach to an existing task, i.e. the main task.
+            /// \param task_to_attach_to The task to attach to.
+            /// \param priority Task priority
+            /// \param tick_interval Tick interval
+            Task(uint32_t priority, std::chrono::milliseconds tick_interval);
 
-                void print_stack_status();
+            /// Use this constructor when creating your own task.
+            /// \param task_name Name of task.
+            /// \param stack_size Tack size, in bytes.
+            /// \param priority Task priority
+            /// \param tick_interval Tick interval
+            /// \param core Core affinity, defaults to no affinity
+            Task(std::string task_name,
+                 uint32_t stack_size,
+                 uint32_t priority,
+                 std::chrono::milliseconds tick_interval,
+                 int core = tskNO_AFFINITY);
 
-                void disable_status_print()
-                { status_print_enabled = false; }
+            /// The tick() method is where the task shall perform its work.
+            /// It is called every 'tick_interval' when there no events available.
+            /// Note that if there is a constant stream of event received via a TaskEventQueue,
+            /// the tick may be delayed (depending on the tick_interval).
+            virtual void tick()
+            {
+            }
 
-                const std::string name;
-            private:
+            /// Called once when task is started.
+            virtual void init()
+            {
+            }
 
-                void exec();
+            void print_stack_status();
 
-                std::thread worker;
-                uint32_t stack_size;
-                uint32_t priority;
-                std::chrono::milliseconds tick_interval;
-                smooth::core::ipc::QueueNotification notification{};
-                bool is_attached;
-                int affinity;
-                std::atomic_bool started{false};
-                std::mutex start_mutex{};
-                std::mutex queue_mutex{};
-                std::condition_variable start_condition{};
-                smooth::core::timer::ElapsedTime status_report_timer{};
-                std::vector<smooth::core::ipc::IPolledTaskQueue *> polled_queues{};
-                bool status_print_enabled = true;
-        };
-    }
+            void disable_status_print()
+            { status_print_enabled = false; }
+
+            const std::string name;
+        private:
+
+            void exec();
+
+            std::thread worker;
+            uint32_t stack_size;
+            uint32_t priority;
+            std::chrono::milliseconds tick_interval;
+            smooth::core::ipc::QueueNotification notification{};
+            bool is_attached;
+            int affinity;
+            std::atomic_bool started{false};
+            std::mutex start_mutex{};
+            std::mutex queue_mutex{};
+            std::condition_variable start_condition{};
+            smooth::core::timer::ElapsedTime status_report_timer{};
+            std::vector<smooth::core::ipc::IPolledTaskQueue*> polled_queues{};
+            bool status_print_enabled = true;
+    };
 }
