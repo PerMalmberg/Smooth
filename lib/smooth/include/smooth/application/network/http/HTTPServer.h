b0f8cea6d27a811d62f6a8efee65c1a519249427
Per Malmberg <per.malmberg@gmail.com>
2019-08-10 15:16:06 +0200

    Handle the case where no custom URL handlers are added and a file is requested.

--- lib/smooth/include/smooth/application/network/http/HTTPServer.h
+++ lib/smooth/include/smooth/application/network/http/HTTPServer.h
@@ -74,32 +74,32 @@ namespace smooth::application::network::http
             {
             }
 
-            const smooth::core::filesystem::Path web_root() const
+            [[nodiscard]] const smooth::core::filesystem::Path web_root() const
             {
                 return root_path;
             }
 
-            const std::vector<std::string>& indexes() const
+            [[nodiscard]] const std::vector<std::string>& indexes() const
             {
                 return index;
             }
 
-            const std::set<std::string>& templates() const
+            [[nodiscard]] const std::set<std::string>& templates() const
             {
                 return template_files;
             }
 
-            std::size_t max_header_size() const
+            [[nodiscard]] std::size_t max_header_size() const
             {
                 return maximum_header_size;
             }
 
-            std::size_t chunck_size() const
+            [[nodiscard]] std::size_t chunck_size() const
             {
                 return content_chunk_size;
             }
 
-            const ITemplateDataRetriever& data_retriever() const
+            [[nodiscard]] const ITemplateDataRetriever& data_retriever() const
             {
                 return template_data_retriever;
             }
@@ -190,6 +190,9 @@ namespace smooth::application::network::http
             void
             reply_with(IServerResponse& response, std::unique_ptr<IResponseOperation> res);
 
+            void serve_file(const HTTPMethod& method, IServerResponse& response, const std::string& requested_url,
+                            const std::unordered_map<std::string, std::string>& request_headers);
+
             smooth::core::Task& task;
             std::shared_ptr<smooth::core::network::ServerSocket<
                     smooth::application::network::http::HTTPServerClient,
@@ -236,93 +239,18 @@ namespace smooth::application::network::http
     {
         using namespace smooth::core::logging;
 
-        // Is there any URLs for the given method?
+        // Is there a URL for the given method?
         auto by_url = handlers.find(method);
 
         if (by_url != handlers.end())
         {
-            // Is there a matching URL?
+            // Is there a specific handler for this URL?
             auto response_handler = (*by_url).second.find(requested_url);
 
             if (response_handler == (*by_url).second.end())
             {
                 // No handler for this URL, does it match a file path beneath the web root?
-
-                auto found = false;
-
-                Log::info(tag,
-                          Format("Request: {1}: '{2}'", Str(utils::http_method_to_string(method)), Str(requested_url)));
-
-                smooth::core::filesystem::Path search{config.web_root()};
-                search /= requested_url;
-
-                if (config.web_root().is_parent_of(search) || config.web_root() == search)
-                {
-                    smooth::core::filesystem::FileInfo info(search);
-
-                    if (info.is_regular_file())
-                    {
-                        // Attempt to process the file as a template.
-                        auto processed_template = template_processor.process_template(info.path());
-
-                        if (processed_template)
-                        {
-                            reply_with(response, std::move(processed_template));
-                        }
-                        else
-                        {
-                            // Not a template, simply serve the requested file
-                            bool send_not_modified = false;
-                            auto if_modified_since = request_headers.find("if-modified-since");
-
-                            if (if_modified_since != request_headers.end())
-                            {
-                                auto since = utils::parse_http_time((*if_modified_since).second);
-
-                                if (since >= info.last_modified_point())
-                                {
-                                    send_not_modified = true;
-                                }
-                            }
-
-                            if (send_not_modified)
-                            {
-                                reply_with(response,
-                                           std::make_unique<responses::ErrorResponse>(ResponseCode::Not_Modified));
-                            }
-                            else
-                            {
-                                reply_with(response, std::make_unique<responses::FileContentResponse>(search));
-                            }
-                        }
-
-                        found = true;
-                    }
-                    else if (info.is_directory())
-                    {
-                        auto index_path = find_index(search);
-                        if (!index_path.empty())
-                        {
-                            auto processed_template = template_processor.process_template(index_path);
-
-                            if (processed_template)
-                            {
-                                reply_with(response, std::move(processed_template));
-                            }
-                            else
-                            {
-                                reply_with(response, std::make_unique<responses::FileContentResponse>(index_path));
-                            }
-
-                            found = true;
-                        }
-                    }
-                }
-
-                if (!found)
-                {
-                    reply_with(response, std::make_unique<responses::ErrorResponse>(ResponseCode::Not_Found));
-                }
+                serve_file(method, response, requested_url, request_headers);
             }
             else
             {
@@ -339,8 +267,89 @@ namespace smooth::application::network::http
         }
         else
         {
-            // No handler for this method type
-            reply_with(response, std::make_unique<responses::ErrorResponse>(ResponseCode::Method_Not_Allowed));
+            // No specific handler for this URL.
+            serve_file(method, response, requested_url, request_headers);
+        }
+    }
+
+    template<typename ServerType>
+    void HTTPServer<ServerType>::serve_file(const HTTPMethod& method, IServerResponse& response, const std::string& requested_url,
+                                const std::unordered_map<std::string, std::string>& request_headers)
+    {
+        auto found = false;
+
+        Log::info(tag,
+                  Format("Request: {1}: '{2}'", Str(utils::http_method_to_string(method)), Str(requested_url)));
+
+        filesystem::Path search{config.web_root()};
+        search /= requested_url;
+
+        if (config.web_root().is_parent_of(search) || config.web_root() == search)
+        {
+            filesystem::FileInfo info(search);
+
+            if (info.is_regular_file())
+            {
+                // Attempt to process the file as a template.
+                auto processed_template = template_processor.process_template(info.path());
+
+                if (processed_template)
+                {
+                    reply_with(response, std::move(processed_template));
+                }
+                else
+                {
+                    // Not a template, simply serve the requested file
+                    bool send_not_modified = false;
+                    auto if_modified_since = request_headers.find("if-modified-since");
+
+                    if (if_modified_since != request_headers.end())
+                    {
+                        auto since = utils::parse_http_time((*if_modified_since).second);
+
+                        if (since >= info.last_modified_point())
+                        {
+                            send_not_modified = true;
+                        }
+                    }
+
+                    if (send_not_modified)
+                    {
+                        reply_with(response,
+                                   std::make_unique<responses::ErrorResponse>(ResponseCode::Not_Modified));
+                    }
+                    else
+                    {
+                        reply_with(response, std::make_unique<responses::FileContentResponse>(search));
+                    }
+                }
+
+                found = true;
+            }
+            else if (info.is_directory())
+            {
+                auto index_path = find_index(search);
+                if (!index_path.empty())
+                {
+                    auto processed_template = template_processor.process_template(index_path);
+
+                    if (processed_template)
+                    {
+                        reply_with(response, std::move(processed_template));
+                    }
+                    else
+                    {
+                        reply_with(response, std::make_unique<responses::FileContentResponse>(index_path));
+                    }
+
+                    found = true;
+                }
+            }
+        }
+
+        if (!found)
+        {
+            reply_with(response, std::make_unique<responses::ErrorResponse>(ResponseCode::Not_Found));
         }
     }
 
