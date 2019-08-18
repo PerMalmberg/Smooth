80257687473534590ce3797fc62b907d5f541942
Per Malmberg <per.malmberg@gmail.com>
2019-08-18 11:35:33 +0200

    All test projects updated to build for Linux and ESP.

--- test/unit_tests/TemplateProcessorTest.cpp
+++ test/unit_tests/TemplateProcessorTest.cpp
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-#include <catch.hpp>
+#include "catch.hpp"
 #include <unordered_map>
 
 #define EXPOSE_PRIVATE_PARTS_FOR_TEST
