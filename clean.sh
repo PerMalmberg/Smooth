5b959527e629b2ab5fea90debd8d44cb31d0e699
Per Malmberg <per.malmberg@gmail.com>
2019-08-18 13:11:01 +0200

    Removed no longer needed scripts.

--- clean.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-pushd `pwd`
-mkdir -p build
-cd build
-cmake .. -G Ninja -DESP_PLATFORM=1 -DCMAKE_TOOLCHAIN_FILE=$IDF_PATH/tools/cmake/toolchain-esp32.cmake -DTARGET=esp32 && \
-popd
--- monitor.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-python $IDF_PATH/tools/idf_monitor.py -p /dev/ttyUSB1 ./build/smooth_test
-
