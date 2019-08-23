73be44eb1bde742ff0b017c9a3132b5c64fff00d
Lothar <springob@gmx.de>
2019-08-23 22:39:37 +0200

    Added support for wifi AP mode to class Wifi (#65)
    Merge PR from @squonk11 - Access Point support in Wifi class.

--- lib/smooth/core/network/Wifi.cpp
+++ lib/smooth/core/network/Wifi.cpp
@@ -22,6 +22,11 @@
 #include <tcpip_adapter.h>
 #include <smooth/core/network/NetworkStatus.h>
 #include <smooth/core/ipc/Publisher.h>
+#include <smooth/core/util/copy_n_to_buffer.h>
+#include <smooth/core/logging/log.h>
+
+using namespace smooth::core;
+using namespace smooth::core::util;
 
 namespace smooth::core::network
 {
@@ -32,8 +37,9 @@ namespace smooth::core::network
 
     Wifi::~Wifi()
     {
-        esp_wifi_disconnect();
-        esp_wifi_stop();
+        ESP_ERROR_CHECK(esp_wifi_disconnect());
+        ESP_ERROR_CHECK(esp_wifi_stop());
+        ESP_ERROR_CHECK(esp_wifi_deinit());
     }
 
     void Wifi::set_host_name(const std::string& name)
@@ -60,13 +66,11 @@ namespace smooth::core::network
         esp_wifi_init(&init);
         esp_wifi_set_mode(WIFI_MODE_STA);
 
-        //QQQ What effect does this setting actually have? esp_wifi_set_auto_connect(true);
-
         wifi_config_t config;
         memset(&config, 0, sizeof(config));
-        strncpy((char*) config.sta.ssid, ssid.c_str(), std::min(sizeof(config.sta.ssid), ssid.length()));
-        strncpy((char*) config.sta.password, password.c_str(),
-                std::min(sizeof(config.sta.password), password.length()));
+        copy_min_to_buffer(ssid.begin(), ssid.length(), config.sta.ssid);
+        copy_min_to_buffer(password.begin(), password.length(), config.sta.password);
+
         config.sta.bssid_set = false;
 
         // Store Wifi settings in RAM - it is the applications responsibility to store settings.
@@ -76,7 +80,7 @@ namespace smooth::core::network
         connect();
     }
 
-    void Wifi::connect()
+    void Wifi::connect() const
     {
         esp_wifi_start();
         esp_wifi_connect();
@@ -104,8 +108,7 @@ namespace smooth::core::network
         }
         else if (event.event_id == SYSTEM_EVENT_STA_DISCONNECTED)
         {
-            network::NetworkStatus status(network::NetworkEvent::DISCONNECTED,
-                                          true);
+            network::NetworkStatus status(network::NetworkEvent::DISCONNECTED, true);
             core::ipc::Publisher<network::NetworkStatus>::publish(status);
 
             connected_to_ap = false;
@@ -115,9 +118,44 @@ namespace smooth::core::network
                 connect();
             }
         }
+        else if (event.event_id == SYSTEM_EVENT_AP_START)
+        {
+            network::NetworkStatus status(network::NetworkEvent::GOT_IP, event.event_info.got_ip.ip_changed);
+            core::ipc::Publisher<network::NetworkStatus>::publish(status);
+        }
+        else if (event.event_id == SYSTEM_EVENT_AP_STACONNECTED)
+        {
+            auto& event_info = event.event_info;
+            Log::info("SoftAP", Format("station MAC: {1}:{2}:{3}:{4}:{5}:{6} join, AID={7}",
+                    Hex(event_info.sta_connected.mac[0]),
+                    Hex(event_info.sta_connected.mac[1]),
+                    Hex(event_info.sta_connected.mac[2]),
+                    Hex(event_info.sta_connected.mac[3]),
+                    Hex(event_info.sta_connected.mac[4]),
+                    Hex(event_info.sta_connected.mac[5]),
+                    UInt32(event_info.sta_connected.aid)));
+        }
+        else if (event.event_id == SYSTEM_EVENT_AP_STADISCONNECTED)
+        {
+            auto& event_info = event.event_info;
+            Log::info("SoftAP", Format("station MAC: {1}:{2}:{3}:{4}:{5}:{6} join, AID={7}",
+                    Hex(event_info.sta_connected.mac[0]),
+                    Hex(event_info.sta_connected.mac[1]),
+                    Hex(event_info.sta_connected.mac[2]),
+                    Hex(event_info.sta_connected.mac[3]),
+                    Hex(event_info.sta_connected.mac[4]),
+                    Hex(event_info.sta_connected.mac[5]),
+                    UInt32(event_info.sta_connected.aid)));
+        }
+        else if (event.event_id == SYSTEM_EVENT_AP_STOP)
+        {
+            Log::info("SoftAP", "AP stopped");
+            network::NetworkStatus status(network::NetworkEvent::DISCONNECTED, true);
+            core::ipc::Publisher<network::NetworkStatus>::publish(status);
+        }
     }
 
-    std::string Wifi::get_mac_address()
+    std::string Wifi::get_mac_address() const
     {
         std::stringstream mac;
 
@@ -135,5 +173,25 @@ namespace smooth::core::network
         }
 
         return mac.str();
+    }
+
+    void Wifi::start_softap(uint8_t max_conn)
+    {
+        wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+        ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+        wifi_config_t config{};
+
+        copy_min_to_buffer(ssid.begin(), ssid.length(), config.ap.ssid);
+        copy_min_to_buffer(password.begin(), password.length(), config.ap.password);
+
+        config.ap.max_connection = max_conn;
+        config.ap.authmode = password.empty() ? WIFI_AUTH_OPEN : WIFI_AUTH_WPA_WPA2_PSK;
+
+        ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));
+        ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_AP, &config));
+        ESP_ERROR_CHECK(esp_wifi_start());
+
+        Log::info("SoftAP", "SSID: " + ssid + "; Auth: " + (password.empty() ? "Open" : "WPA2/PSK"));
     }
 }
---
+++ test/access_point/access_point.cpp
@@ -0,0 +1,120 @@
+// Smooth - C++ framework for writing applications based on Espressif's ESP-IDF.
+// Copyright (C) 2017 Per Malmberg (https://github.com/PerMalmberg)
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+#include "access_point.h"
+#include <memory>
+#include <smooth/core/logging/log.h>
+#include <smooth/core/task_priorities.h>
+#include <smooth/core/network/IPv4.h>
+#include <smooth/core/filesystem/filesystem.h>
+#include <smooth/core/filesystem/FSLock.h>
+#include <smooth/application/network/http/regular/responses/StringResponse.h>
+#include <smooth/application/network/http/regular/MIMEParser.h>
+#include <smooth/core/SystemStatistics.h>
+
+#include "wifi_creds.h"
+
+using namespace std;
+using namespace std::chrono;
+using namespace smooth::core;
+using namespace smooth::core::filesystem;
+using namespace smooth::core::network;
+using namespace smooth::core::network::event;
+using namespace smooth::core::logging;
+using namespace smooth::application::network::http;
+using namespace smooth::application::network::http::responses;
+
+namespace access_point
+{
+
+    App::App()
+            : Application(smooth::core::APPLICATION_BASE_PRIO, std::chrono::milliseconds(5000))
+    {
+    }
+
+    void App::tick()
+    {
+        smooth::core::SystemStatistics::instance().dump();
+    }
+
+    void App::init()
+    {
+        std::stringstream ss;
+
+        const int max_client_count = 6;
+        const int listen_backlog = 6;
+
+        Application::init();
+#ifdef ESP_PLATFORM
+        Log::info("App::Init", Format("Starting wifi..."));
+        network::Wifi& wifi = get_wifi();
+        wifi.set_host_name("Smooth-ESP");
+        wifi.set_ap_credentials(WIFI_SSID, WIFI_PASSWORD);
+        wifi.start_softap(2);
+
+        Path web_root(FlashMount::instance().mount_point() / "web_root");
+
+        flash = std::make_unique<smooth::core::filesystem::SPIFlash> (smooth::core::filesystem::FlashMount::instance(),
+                                                     "app_storage",
+                                                     10,
+                                                     true);
+        flash->mount();
+
+#else
+        const smooth::core::filesystem::Path test_path(__FILE__);
+        const smooth::core::filesystem::Path web_root = test_path.parent() / "web_root";
+        Path uploads{test_path.parent() / "uploads"};
+#endif
+
+        HTTPServerConfig cfg{web_root, {"index.html"}, {".html"}, nullptr, MaxHeaderSize,
+                             ContentChunkSize};
+
+        insecure_server = std::make_unique<InsecureServer>(*this, cfg);
+
+        insecure_server->start(max_client_count, listen_backlog, std::make_shared<IPv4>("0.0.0.0", 8080));
+
+        auto hello_world = [](
+                IServerResponse& response,
+                IConnectionTimeoutModifier& timeout_modifier,
+                const std::string& url,
+                bool first_part,
+                bool last_part,
+                const std::unordered_map<std::string, std::string>& headers,
+                const std::unordered_map<std::string, std::string>& request_parameters,
+                const std::vector<uint8_t>& content,
+                MIMEParser& mime) {
+            (void) timeout_modifier;
+            (void) url;
+            (void) first_part;
+            (void) headers;
+            (void) content;
+            (void) mime;
+
+            if (last_part)
+            {
+                response.reply(std::make_unique<responses::StringResponse>(
+                               ResponseCode::OK,
+                               "<HTML><HEAD><TITLE>Hello World!</TITLE></HEAD><BODY><H1>Hello World!</H1></BODY></HTML>"),
+                               false);
+            }
+        };
+
+        // As there's no actual index file in this example, use a response handles instead when calling requesting "/".
+        // See http_server_test to see how a web server with actual files is set up.
+        insecure_server->on(HTTPMethod::GET, "/", hello_world);
+    }
+}
+
