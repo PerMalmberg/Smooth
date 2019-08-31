bd7ed0588988c5f2a313c266da1c43fcb631357a
Per Malmberg <per.malmberg@gmail.com>
2019-08-31 10:28:58 +0200

    #63 - json_test builds again.

--- lib/smooth/core/json/JsonFile.cpp
+++ lib/smooth/core/json/JsonFile.cpp
@@ -15,11 +15,8 @@
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #include <smooth/core/json/JsonFile.h>
-#include <cJSON.h>
-
 #include <memory>
 #include <vector>
-#include <iostream>
 
 namespace smooth::core::json
 {
