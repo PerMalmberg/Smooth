8257d53f1e097082ac6c4541384faeee9c800b93
Per Malmberg <per.malmberg@gmail.com>
2020-04-12 19:32:02 +0200

    More Docker stuff

--- CI/build_test.sh
+++ CI/build_test.sh
@@ -11,23 +11,24 @@ run_in_docker() {
     echo "Missing command for docker"
     exit 1
   else
-    docker run -u "$(id -u)" --rm --mount type=bind,source="$start_dir",target=$source_root "$@"
+    docker run --rm --mount type=bind,source="$start_dir",target=$source_root "$@"
   fi
 }
 
 build_host() {
-  run_in_docker --workdir $source_root $docker_image $source_root/CI/container_scripts/prepare_build_dir.sh
-
-  run_in_docker --workdir $source_root $docker_image $source_root/CI/container_scripts/build_smooth_host.sh
+  run_in_docker --workdir "$source_root" "$docker_image" "$source_root/CI/container_scripts/prepare_build_dir.sh"
+  run_in_docker --workdir "$source_root" "$docker_image" "$source_root/CI/container_scripts/build_smooth_host.sh"
+  run_in_docker --workdir "$source_root" "$docker_image" "$source_root/CI/container_scripts/set_build_dir_perms.sh"
 }
 
 run_host_tests() {
-  run_in_docker --workdir $source_root/build/test/linux_unit_tests $docker_image ./linux_unit_tests
+  run_in_docker --workdir "$source_root/build/test/linux_unit_tests" "$docker_image" "./linux_unit_tests"
 }
 
 build_esp32_test_projects() {
   run_in_docker --workdir "$source_root" "$docker_image" "$source_root/CI/container_scripts/prepare_build_dir.sh"
-  run_in_docker --workdir /src permalmberg/smooth:latest /src/CI/container_scripts/build_smooth_esp32.sh
+  run_in_docker --workdir "$source_root" "$docker_image" "$source_root/CI/container_scripts/build_smooth_esp32.sh"
+  run_in_docker --workdir "$source_root" "$docker_image" "$source_root/CI/container_scripts/set_build_dir_perms.sh"
 }
 
 build_host
--- CI/docker_info.sh
+++ CI/docker_info.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker_image="permalmberg/smooth:30372f5a4ff2c0dfdaad95f544dc36bcdda30b75"
+docker_image="permalmberg/smooth:latest"
 uncrustify_image="permalmberg/uncrustify:0.70.0"
 
 export docker_image
